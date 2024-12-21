50ac2548e19cd07f3b9241457d780fcf14c98856
Gabriela Rodrigues de Souza <gabriela.souza.investimentos@gmail.com>
2024-12-21 12:09:13 -0300

    Terceira Versão- Adicionando Métodos na Interface e aplicando ações em seus botões

--- src/supermercado/view/ProdutoView.java
+++ src/supermercado/view/ProdutoView.java
@@ -23,6 +23,8 @@ public class ProdutoView extends javax.swing.JFrame {
         textPreco = new javax.swing.JTextField();
         btnSalvar = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        btnExcluir = new javax.swing.JButton();
+        btnAtualizar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -56,6 +58,24 @@ public class ProdutoView extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Sitka Small", 0, 36)); // NOI18N
         jLabel3.setText("Cadastrar Produto");
 
+        btnExcluir.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
+        btnExcluir.setText("Excluir");
+        btnExcluir.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        btnExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcluirActionPerformed(evt);
+            }
+        });
+
+        btnAtualizar.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
+        btnAtualizar.setText("Editar");
+        btnAtualizar.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        btnAtualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAtualizarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -68,19 +88,25 @@ public class ProdutoView extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(61, 61, 61)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(18, 18, 18)
-                                .addComponent(textPreco))
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel1)
                                 .addGap(18, 18, 18)
-                                .addComponent(textNome, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(textNome, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(18, 18, 18)
+                                .addComponent(textPreco)))))
                 .addContainerGap(177, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
+                .addGap(83, 83, 83)
+                .addComponent(btnExcluir, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnAtualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(82, 82, 82))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(302, 302, 302))
+                .addGap(304, 304, 304))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,9 +121,13 @@ public class ProdutoView extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(textPreco, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 107, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
                 .addComponent(btnSalvar)
-                .addGap(87, 87, 87))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnExcluir)
+                    .addComponent(btnAtualizar))
+                .addGap(77, 77, 77))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -125,6 +155,14 @@ public class ProdutoView extends javax.swing.JFrame {
     private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
         salvarProduto();
     }//GEN-LAST:event_btnSalvarActionPerformed
+
+    private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirActionPerformed
+        excluirProduto();
+    }//GEN-LAST:event_btnExcluirActionPerformed
+
+    private void btnAtualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAtualizarActionPerformed
+        editarProduto();
+    }//GEN-LAST:event_btnAtualizarActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -159,6 +197,8 @@ public class ProdutoView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAtualizar;
+    private javax.swing.JButton btnExcluir;
     private javax.swing.JButton btnSalvar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -215,5 +255,64 @@ public class ProdutoView extends javax.swing.JFrame {
     {
         textNome.setText("");
         textPreco.setText("");
+    }
+
+    private void excluirProduto() 
+    {
+        try
+        {
+            int id = Integer.parseInt(JOptionPane.showInputDialog(this, "Digite o ID do produto para excluir: "));
+            
+            ProdutoDAO dao = new ProdutoDAO();
+            
+            int confirmacao = JOptionPane.showConfirmDialog(this, "Tem certeza que deseja excluir o produto?",
+                                                                  "Confirmação ", JOptionPane.YES_NO_OPTION);
+            
+            if(confirmacao == JOptionPane.YES_OPTION)
+            {
+                if(dao.excluirProduto(id))
+                {
+                    JOptionPane.showMessageDialog(this, "Produto excluído com sucesso!");
+                }
+                
+                else
+                {
+                    JOptionPane.showMessageDialog(this, "Erro ao excluir produto!");
+                }
+            }
+        }
+        
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(this, "Erro: " + e.getMessage());
+        }
+    }
+
+    private void editarProduto() 
+    {
+        try
+        {
+            int id= Integer.parseInt(JOptionPane.showInputDialog(this, "Digite o ID do produto para atualizar: "));
+            String novoNome = JOptionPane.showInputDialog(this, "Novo nome do produto: ");
+            double novoPreco = Double.parseDouble(JOptionPane.showInputDialog(this, "Novo preço do produto: "));
+            
+            ProdutoEntity produto = new ProdutoEntity(id, novoNome, novoPreco);
+            ProdutoDAO dao = new ProdutoDAO();
+            
+            if(dao.atualizarProduto(produto))
+            {
+                JOptionPane.showMessageDialog(this, "Produto atualizado com sucesso!");
+            }
+            
+            else
+            {
+                JOptionPane.showMessageDialog(this, "Erro ao atualizar produto!");
+            }
+        }
+        
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(this, "Erro: " + e.getMessage());
+        }
     }
 }
